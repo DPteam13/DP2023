9ea7be82df81c0452d8c7db58b67b0ffe6aa0b7a
newmin9999 <mihnhyuk@naver.com>
2023-11-08 16:37:46 +0900

    html exporter

--- Holup/src/com/holub/database/HTMLExporter.java
+++ Holup/src/com/holub/database/HTMLExporter.java
@@ -4,20 +4,40 @@ import java.io.*;
 import java.util.Iterator;
 
 public class HTMLExporter implements Table.Exporter{
-	public void startTable()			throws IOException{
+	private final Writer out;
 
-	};
+	private		int		width;
+
+	public HTMLExporter( Writer out )
+	{	this.out = out;
+	}
+
+
 	public void storeMetadata(
 			String tableName,
 			int width,
 			int height,
-			Iterator columnNames )	throws IOException{
+			Iterator columnNames )	throws IOException
+	{	this.width = width;
+		out.write(tableName == null ? "<table>" : tableName );
+		out.write("\n");
+		storeRow( columnNames ); // comma separated list of columns ids
+		out.write(tableName == null ? "</table>" : tableName);
+	};
+	public void storeRow(Iterator data) throws IOException
+	{	int i = width;
+		out.write("<tr><td>");
+		while( data.hasNext())
+		{	Object datum = data.next();
+			if( datum != null)
+				out.write(datum.toString());
+			if(--i > 0) {
+				out.write("</td><td>");
+			}
+		}
+		out.write("</td></tr>");
 
 	};
-	public void storeRow(Iterator data) throws IOException{
-
-	};
-	public void endTable()			 	throws IOException{
-
-	};
+	public void startTable() throws IOException{/* nothing to do*/};
+	public void endTable()	throws IOException{/* nothing to do*/};
 }
