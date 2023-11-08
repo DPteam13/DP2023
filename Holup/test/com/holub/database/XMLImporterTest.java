540c4a8e3910e6289b798b03457fe825587a9db0
Joo98 <hynciath51@gmail.com>
2023-11-08 20:45:33 +0900

    add XMLImportTest class

---
+++ Holup/test/com/holub/database/XMLImporterTest.java
@@ -0,0 +1,25 @@
+package com.holub.database;
+import org.xml.sax.SAXException;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class XMLImporterTest {
+    public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
+
+        String file = "people.xml"; //read people.xml 
+        XMLImporter builder1 = new XMLImporter(file);
+        builder1.startTable();
+        assertThat(builder1.loadTableName(), is(equalTo("people")));
+        assertThat(builder1.loadWidth(), is(equalTo(3)));
+
+        String file2 = "university.xml"; //read university.xml
+        XMLImporter builder2 = new XMLImporter(file2);
+        builder2.startTable();
+        assertThat(builder2.loadTableName(), is(equalTo("university")));
+        assertThat(builder2.loadWidth(), is(equalTo(2)));
+    }
+
+}
